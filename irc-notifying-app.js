fbb001d9a0e4dcee736a6abccc10a2912fa230b0
Atul Varma <varmaa@gmail.com>
2012-06-18 10:11:07 -0400

    fixed irc code so it doesn't duplicate msgs when someone joins the room.

--- irc-notifying-app.js
+++ irc-notifying-app.js
@@ -13,30 +13,31 @@ var client = new irc.Client(config.irc.server, config.irc.username, {
     channels: [config.irc.channel]
 });
 
-client.on("join", function() {
-  function say(msg) {
+function say(msg) {
+  try {
     client.say(config.irc.channel, msg);
+  } catch (e) {
+    console.log("failed to broadcast on IRC: " + e);
   }
-  
-  //client.say(config.irc.channel, "testing makes us stronger!");
-  app.on("job-submitted", function(info) {
-    var shortname = info.account + "/" + info.name + "@" +
-        info.commit.slice(0, 7);
-    say("tests for " + shortname +
-        " started on " + info.capabilities.length + " browsers. " +
-        "For detailed results, see " + config.baseURL + ".");
-  });
-  
-  app.on("webdriver-session-finished", function(entry) {
-    var caps = entry.capabilities;
-    var browserName = BROWSER_NAMES[caps.browserName] +
-                      (caps.version ? ' ' + caps.version : '')
-    if (entry.result) {
-      say(entry.result.passed + " of " + entry.result.total +
-          " tests passed on " + browserName + ".");
-    } else
-      say("All tests failed on " + browserName + ".");
-  });
+}
+
+app.on("job-submitted", function(info) {
+  var shortname = info.account + "/" + info.name + "@" +
+      info.commit.slice(0, 7);
+  say("tests for " + shortname +
+      " started on " + info.capabilities.length + " browsers. " +
+      "For detailed results, see " + config.baseURL + ".");
+});
+
+app.on("webdriver-session-finished", function(entry) {
+  var caps = entry.capabilities;
+  var browserName = BROWSER_NAMES[caps.browserName] +
+                    (caps.version ? ' ' + caps.version : '');
+  if (entry.result) {
+    say(entry.result.passed + " of " + entry.result.total +
+        " tests passed on " + browserName + ".");
+  } else
+    say("All tests failed on " + browserName + ".");
 });
 
 app.autoListen();
